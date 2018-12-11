faf83f53e46828f972a6fd7987bab35600efa5bf
Haroldo Ramirez <haroldoramirez@gmail.com>
2018-12-10 23:38:43 -0200

    first commit

---
+++ src/main/java/org/cibiogas/cibchartsapp/CibchartsappApplication.java
@@ -0,0 +1,12 @@
+package org.cibiogas.cibchartsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibchartsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibchartsappApplication.class, args);
+	}
+}
---
+++ src/main/java/org/cibiogas/cibchartsapp/DashboardController.java
@@ -0,0 +1,14 @@
+package org.cibiogas.cibchartsapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DashboardController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "dashboard.html";
+	}
+
+}
