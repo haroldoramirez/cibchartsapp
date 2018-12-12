8a42b394d49f2c9ca98ede0461f0a13f63b04487
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2018-12-12 11:40:41 -0200

    configurado as paginas para acessar os dados via csv e partindo para configurar o spring como executavel

--- src/main/java/org/cibiogas/cibchartsapp/CibchartsappApplication.java
+++ src/main/java/org/cibiogas/cibchartsapp/CibchartsappApplication.java
@@ -2,9 +2,12 @@ package org.cibiogas.cibchartsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
-public class CibchartsappApplication {
+public class CibchartsappApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CibchartsappApplication.class, args);
